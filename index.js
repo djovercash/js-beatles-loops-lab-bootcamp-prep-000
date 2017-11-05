68574a18dafa3b56b342826d5f1f80173901de9f
Drew Overcash <drewovercash@gmail.com>
2017-11-05 18:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function johnLennonFacts (facts) {
 function iLoveTheBeatles (n) {
   var loveArr = [];
   do {
-    arr.push(`I love the Beatles!`);
+    loveArr.push(`I love the Beatles!`);
     n++;
   } while (n < 15);
   return loveArr;
