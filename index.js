c977f02af92a55f741e7dbaadca931e422f51079
Drew Overcash <drewovercash@gmail.com>
2017-11-05 18:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay (array, instruments) {
   var beatlesArr = [];
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < muscians.length; i++) {
     beatlesArr.push(`${musicians[i]} plays ${instruments[i]}`)
   };
+  return beatlesArr;
